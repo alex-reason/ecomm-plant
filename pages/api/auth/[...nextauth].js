bb79741dfb9ce500dbf310488fe8a01907740e0c
Alecs Reasonda <momai.reason@gmail.com>
2023-09-06 14:01:28 -0400

    added mongodb as adapter for next auth; set up mongodb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,11 @@
 import NextAuth from 'next-auth';
+// providers
 import GoogleProvider from 'next-auth/providers/google';
 // import EmailProvider from 'next-auth/providers/email';
+
+// MongoDB adapter
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "../../../lib/mongodb"
 
 export default NextAuth({
   providers: [
@@ -14,5 +19,6 @@ export default NextAuth({
     //   server: process.env.MAIL_SERVER,
     //   from: 'NextAuth.js <no-reply@example.com>'
     // }),
-  ]
+  ],
+  adapter: MongoDBAdapter(clientPromise),
 });
